33e525299e86118b770bf78691bdc24670af7cea
rayannebandeira <rkbr1@aluno.ifnmg.edu.br>
2026-04-08 16:49:15 +0000

    feat:pratica fundamentos de JavaScript no Node - aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -9,28 +9,49 @@
 export function classificarIdade(idade) {
   if (idade > 12 ){
     return "criança";
-  }else if (idade >= 12<= && 17){
+  }else if (idade >= 12 && idade <= 17){
     return "adolescente";
   }else{
-  return "adulto"
+  return "adulto"}
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  
+  return preco - (preco * percentual / 100);
 }
+
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+  if (nota >=60){
+    return "aprovado";
+  }else if (nota >=40 && nota <60){
+    return "recuperação";
+  }else{
+    return "reprovado"; 
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+  let soma = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      soma += numeros[i];
+    }
+  }
+  return soma;
+  
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contador = 0;
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] === letra) {
+      contador++;
+    }
+  }
+  return contador;
+
 }
